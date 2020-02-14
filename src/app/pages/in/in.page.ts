941be88e68429e1e4b7ced7d740e4df5f029d5c7
Camilo Muñoz <jmunoz@lean-tech.io>
2020-02-14 10:51:49 -0500

    Listing mock tasks for "next" and "in"

---
+++ src/app/pages/in/in.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-in',
+  templateUrl: './in.page.html',
+  styleUrls: ['./in.page.scss'],
+})
+export class InPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
