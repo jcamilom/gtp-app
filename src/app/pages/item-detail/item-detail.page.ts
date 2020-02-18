d55b32ecf401679daa6cf337e54111809b2fabac
Camilo Muñoz <jmunoz@lean-tech.io>
2020-02-18 15:40:43 -0500

    Added basic item form

--- src/app/pages/item-detail/item-detail.page.ts
+++ src/app/pages/item-detail/item-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item-detail',
@@ -8,17 +9,32 @@ import { Router } from '@angular/router';
 })
 export class ItemDetailPage implements OnInit {
 
+  public form: FormGroup;
+
   item: string;
   mode: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    const formValue = { title: '', description: '' };
     const navigation = this.router.getCurrentNavigation();
     if (navigation.extras.state) {
       this.item = navigation.extras.state.item;
       this.mode = navigation.extras.state.mode;
+      if (this.mode === 'edit') {
+        formValue.title = 'title';
+        formValue.description = 'anything';
+      }
     }
+    this.form = this.fb.group({
+      title: [formValue.title, [Validators.required]],
+      description: formValue.description
+    });
+  }
+
+  public submitForm(): void {
+
   }
 
 }
