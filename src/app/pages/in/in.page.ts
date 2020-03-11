490fa613071c2470dedc7b7ba127c499ef9f5066
Camilo Muñoz <jcamilomunozz@gmail.com>
2020-03-11 16:37:37 -0500

    Moving items in "in" page

--- src/app/pages/in/in.page.ts
+++ src/app/pages/in/in.page.ts
@@ -3,10 +3,12 @@ import { Router } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import 'firebase/firestore';
 import { Observable } from 'rxjs';
+import { ActionSheetController } from '@ionic/angular';
 
 export interface GeneralItem {
   id: string;
   title: string;
+  state: string;
   description?: string;
 }
 
@@ -20,7 +22,11 @@ export class InPage implements OnInit {
   private itemsCollection: AngularFirestoreCollection<GeneralItem>;
   public items: Observable<GeneralItem[]>;
 
-  constructor(private router: Router, private firestore: AngularFirestore) { }
+  constructor(
+    private router: Router,
+    private firestore: AngularFirestore,
+    private actionSheetController: ActionSheetController
+  ) { }
 
   ngOnInit() {
     this.itemsCollection = this.firestore.collection<GeneralItem>('items', ref => ref.where('state', '==', 'in'));
@@ -31,15 +37,33 @@ export class InPage implements OnInit {
     this.router.navigate(['/item-detail'], { state: { item, mode: 'create' } });
   }
 
-  public moveItem(item: GeneralItem): void {
-  }
-
   public editItem(item: GeneralItem): void {
     this.router.navigate(['/item-detail'], { state: { item, mode: 'edit' } });
   }
 
   public deleteItem(item: GeneralItem): void {
     this.itemsCollection.doc<GeneralItem>(item.id).delete();
+  }
+
+  public async moveItem(item: GeneralItem): Promise<any> {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Where to?',
+      buttons: [
+        {
+          text: 'Next',
+          icon: 'rocket-outline',
+          handler: () => {
+            this.itemsCollection.doc<GeneralItem>(item.id).update({ state: 'next' }).then(() => { });
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+        }
+      ]
+    });
+    await actionSheet.present();
   }
 
 }
