4e4611f417e74a1dab02c194388593152be03a46
Camilo Muñoz <jcamilomunozz@gmail.com>
2020-06-25 23:22:47 -0500

    Added basic register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/auth/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/pages/auth/register/register.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  public form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  public submitForm(): void {
+    console.log(this.form.value);
+  }
+
+}
