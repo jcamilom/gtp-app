68d9d223b6dacb28220aee3884907a19ca82a262
Camilo Muñoz <jcamilomunozz@gmail.com>
2020-06-26 00:05:03 -0500

    Added validations to register form

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+// tslint:disable-next-line: max-line-length
+const emailRegExp: RegExp = /^(([^<>()\[\]\\.,;:\s@'+|={}`"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -16,13 +19,18 @@ export class RegisterPage implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ['', [Validators.required, Validators.pattern(emailRegExp)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
   public submitForm(): void {
     console.log(this.form.value);
   }
+
+  get email() { return this.form.get('email'); }
+
+  get password() { return this.form.get('password'); }
+
 
 }
